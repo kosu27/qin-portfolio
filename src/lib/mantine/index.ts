e65af4cfd18ac8dfa63a4e2471aab11611747bec
kosu <kosumon27@gmail.com>
2022-08-08 17:18:18 +0900

    🔧 Mantine + Tailwindcss

---
+++ src/lib/mantine/index.ts
@@ -0,0 +1,2 @@
+export { useMediaQuery } from "./useMediaQuery";
+export { Button } from "./Button";
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "src/lib/tailwind.css";
 import type { AppProps } from "next/app";
 import { MantineProvider } from "@mantine/core";
 
