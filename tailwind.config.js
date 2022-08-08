45ca5659d56dfbdd4a7b9f58e76c80fb564488c7
kosu <kosumon27@gmail.com>
2022-08-08 17:01:18 +0900

    ➕ Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/line-clamp"),
+  ],
+  corePlugins: {
+    preflight: false,
+  },
+};
