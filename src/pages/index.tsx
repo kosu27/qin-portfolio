a15a3d406e946c3456a18affeecec6c8fed65388
kosu <kosumon27@gmail.com>
2022-08-11 16:40:29 +0900

    💄

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,21 @@
+import { AppShell, useMantineTheme } from "@mantine/core";
 import type { NextPage } from "next";
-import { Button, useMediaQuery, useViewportSize } from "src/lib/mantine";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  const { width } = useViewportSize();
-  const largerThanXs = useMediaQuery("xs");
-  const largerThanSm = useMediaQuery("sm");
-  const largerThanMd = useMediaQuery("md");
-  const largerThanLg = useMediaQuery("lg");
-  const largerThanXl = useMediaQuery("xl");
+  const theme = useMantineTheme();
+  const [opened, setOpened] = useState(false);
 
   return (
-    <div className="p-20">
-      <div className="bg-fuchsia-200 xs:bg-red-200 sm:bg-amber-200 md:bg-lime-200 lg:bg-emerald-200 xl:bg-cyan-200">
-        <div>{`width: ${width}`}</div>
-        <div>{`largerThanXs: ${largerThanXs}`}</div>
-        <div>{`largerThanSm: ${largerThanSm}`}</div>
-        <div>{`largerThanMd: ${largerThanMd}`}</div>
-        <div>{`largerThanLg: ${largerThanLg}`}</div>
-        <div>{`largerThanXl: ${largerThanXl}`}</div>
-      </div>
-      <Button dent className="mt-4 block">
-        Click
-      </Button>
-      <Button className="mt-4 block">Click!</Button>
+    <div>
+      <AppShell
+        styles={{
+          main: {
+            backgroundColor:
+              theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
+          },
+        }}
+      ></AppShell>
     </div>
   );
 };
