880ed32fbbba77071158ed7780abac763a6f8674
kosu <kosumon27@gmail.com>
2022-08-12 19:56:29 +0900

    💄

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
-import { AppShell, useMantineTheme } from "@mantine/core";
+import {
+  AppShell,
+  Aside,
+  Burger,
+  Footer,
+  Header,
+  MediaQuery,
+  Navbar,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
 import type { NextPage } from "next";
 import { useState } from "react";
 
@@ -15,7 +25,45 @@ const Home: NextPage = () => {
               theme.colorScheme === "dark" ? theme.colors.dark[8] : theme.colors.gray[0],
           },
         }}
-      ></AppShell>
+        navbarOffsetBreakpoint="sm"
+        asideOffsetBreakpoint="sm"
+        navbar={
+          <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
+            <Text>Kosu IT University</Text>
+          </Navbar>
+        }
+        aside={
+          <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
+            <Aside p="md" hiddenBreakpoint="sm" width={{ sm: 200, lg: 300 }}>
+              <Text>Sidebar</Text>
+            </Aside>
+          </MediaQuery>
+        }
+        footer={
+          <Footer height={60} p="md">
+            © 2022 Kosu IT University
+          </Footer>
+        }
+        header={
+          <Header height={70} p="md">
+            <div style={{ display: "flex", alignItems: "center", height: "100%" }}>
+              <MediaQuery largerThan="sm" styles={{ display: "none" }}>
+                <Burger
+                  opened={opened}
+                  onClick={() => setOpened((o) => !o)}
+                  size="sm"
+                  color={theme.colors.gray[6]}
+                  mr="xl"
+                />
+              </MediaQuery>
+
+              <Text>Header</Text>
+            </div>
+          </Header>
+        }
+      >
+        <Text>Resize app to see responsive navbar in action</Text>
+      </AppShell>
     </div>
   );
 };
