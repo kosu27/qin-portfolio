f43e20fa805e8ef34627c5e7865ed194aae0624f
kosu <kosumon27@gmail.com>
2022-08-14 16:26:57 +0900

    🔥

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,8 @@
-import {
-  AppShell,
-  Aside,
-  Burger,
-  Footer,
-  MediaQuery,
-  Navbar,
-  Text,
-  useMantineTheme,
-} from "@mantine/core";
 import type { NextPage } from "next";
-import { useState } from "react";
-import { Header } from "src/components/Header";
-import { TitleZone } from "src/components/TitleZone";
+import { Header } from "components/Header";
+import { TitleZone } from "components/TitleZone";
 
 const Home: NextPage = () => {
-  const theme = useMantineTheme();
-  const [opened, setOpened] = useState(false);
-
   return (
     <div>
       <Header />
