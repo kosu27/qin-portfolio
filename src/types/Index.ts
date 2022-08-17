a9019d4ad0e957b8e217e9e4c62945746cad4602
kosu <kosumon27@gmail.com>
2022-08-17 17:23:29 +0900

    🏷️ type

--- src/types/Index.ts
+++ src/types/Index.ts
@@ -1,4 +1,6 @@
 import { Blog } from "types/Blog";
 import { Portfolio } from "types/Portfolio";
+import { Github, Languages } from "types/Github";
+import { Language } from "types/Language";
 
-export type { Blog, Portfolio };
+export type { Blog, Portfolio, Github, Language, Languages };
