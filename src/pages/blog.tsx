283476fb4e386d05433785e13356de9fd04c3cee
kosu <kosumon27@gmail.com>
2022-08-18 15:16:40 +0900

    🩹 ブログページ修正

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,6 @@
 import { Blogs } from "components/Blog";
 import { Layout } from "components/templates/Layout";
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import { Blog } from "types/Blog";
 
 type Props = {
@@ -13,6 +13,23 @@ const Blog: NextPage<Props> = ({ blogs }) => {
       <Blogs blogs={blogs} isGeneral={true} />
     </Layout>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const blogs: Blog[] = Array.from(new Array(10)).map((_, i) => ({
+    id: i + 1,
+    title: "This is a header",
+    contents:
+      "Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.",
+    created_at: "2022/7/11",
+  }));
+
+  return {
+    props: {
+      blogs: blogs,
+    },
+    revalidate: 60,
+  };
 };
 
 export default Blog;
