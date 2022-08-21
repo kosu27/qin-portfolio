93ada01016733e377b3bae014454f59897aca9d1
kosu <kosumon27@gmail.com>
2022-08-21 20:33:15 +0900

    💄 Progress

--- src/components/Github/GithubItem.tsx
+++ src/components/Github/GithubItem.tsx
@@ -1,4 +1,4 @@
-import { Group, Stack, Text } from "@mantine/core";
+import { ColorSwatch, Group, Progress, Stack, Text, useMantineTheme } from "@mantine/core";
 import { GithubData } from "components/Github/GithubData";
 import { FC } from "react";
 import { Github } from "types/Github";
@@ -8,6 +8,7 @@ type Props = {
 };
 
 export const GithubItem: FC<Props> = ({ github }) => {
+  const theme = useMantineTheme();
   return (
     <Stack spacing={4}>
       <Text size={20} weight="bold">
@@ -17,6 +18,43 @@ export const GithubItem: FC<Props> = ({ github }) => {
       <Group spacing={20}>
         <GithubData name="star" value={117} />
         <GithubData name="fork" value={18} />
+      </Group>
+
+      <Progress
+        sections={[
+          { value: 65.5, color: "#3178C6" },
+          { value: 33.7, color: "#F1E05A" },
+          { value: 0.8, color: "#EDEDED" },
+        ]}
+      />
+      <Group spacing={16}>
+        <Group spacing={6}>
+          <ColorSwatch color="#3178C6" size={6} />
+          <Text size="xs" weight={700}>
+            TypeScript
+          </Text>
+          <Text size="xs" color={theme.colors.dark[2]} weight={700}>
+            {65.5}%
+          </Text>
+        </Group>
+        <Group spacing={6}>
+          <ColorSwatch color="#F1E05A" size={6} />
+          <Text size="xs" weight={700}>
+            JavaScript
+          </Text>
+          <Text size="xs" color={theme.colors.dark[2]} weight={700}>
+            {33.7}%
+          </Text>
+        </Group>
+        <Group spacing={6}>
+          <ColorSwatch color="#EDEDED" size={6} />
+          <Text size="xs" weight={700}>
+            Other
+          </Text>
+          <Text size="xs" color={theme.colors.dark[2]} weight={700}>
+            {0.8}%
+          </Text>
+        </Group>
       </Group>
     </Stack>
   );
