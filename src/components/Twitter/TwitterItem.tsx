aeb6ffa2e5a234b5ef9826e69f7d9c994a9157fe
kosu <kosumon27@gmail.com>
2022-08-22 04:50:47 +0900

    ♻️ Twitter関連のリファクタ

--- src/components/Twitter/TwitterItem.tsx
+++ src/components/Twitter/TwitterItem.tsx
@@ -1,4 +1,11 @@
-import { Avatar, Group, Stack, Text, TypographyStylesProvider } from "@mantine/core";
+import {
+  Avatar,
+  Group,
+  Stack,
+  Text,
+  TypographyStylesProvider,
+  useMantineTheme,
+} from "@mantine/core";
 import { FC } from "react";
 import { Twitter } from "types/Twitter";
 
@@ -7,24 +14,29 @@ type Props = {
 };
 
 export const TwitterItem: FC<Props> = ({ twitter }) => {
+  const theme = useMantineTheme();
   return (
     <Group spacing={16} align="start" noWrap={true}>
-      <Avatar src={twitter.userIcon} radius="lg" />
+      <Avatar src={twitter.userIcon} radius="xl" />
       <Stack spacing={4}>
         <Group spacing={8}>
           <Text size={16} weight="bold">
             {twitter.userName}
           </Text>
-          <Text>{`@${twitter.userId}・${twitter.createdAt}`}</Text>
-          <TypographyStylesProvider>
-            <div
-              dangerouslySetInnerHTML={{
-                __html:
-                  "<p>📣 新サービス「Noway Form」をリリースしました！</p><p>Noway Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogle FormsでやっていたことがNotionだけで完結します✌✨</p><p>試しに使っていただけると幸いです😊</p><p><a>https://www.noway-form.com/ja</a></p>",
-              }}
-            />
-          </TypographyStylesProvider>
+          <Text
+            size={12}
+            weight="bold"
+            color={theme.colors.dark[2]}
+          >{`@${twitter.userId}・${twitter.createdAt}`}</Text>
         </Group>
+        <TypographyStylesProvider>
+          <div
+            dangerouslySetInnerHTML={{
+              __html:
+                "<p>📣 新サービス「Noway Form」をリリースしました！</p><p>Noway Formは、Notionのデータベースをもとにフォームを作成できるサービスです。これまでGoogle FormsでやっていたことがNotionだけで完結します✌✨</p><p>試しに使っていただけると幸いです😊</p>< p > <a>https://www.noway-form.com/ja</a></p>",
+            }}
+          />
+        </TypographyStylesProvider>
       </Stack>
     </Group>
   );
