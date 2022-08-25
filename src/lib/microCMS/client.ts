6f5d9da7913dc35d1b9c3f033e3d20b19a002fec
kosu <kosumon27@gmail.com>
2022-08-25 20:19:37 +0900

    ➕ dayjs導入

--- src/lib/microCMS/client.ts
+++ src/lib/microCMS/client.ts
@@ -3,5 +3,5 @@ import { createClient } from "microcms-js-sdk"; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: "qinportfolio", // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: "1b42bfe7ca234128bd9f2aaea250bac7bc4d",
+  apiKey: process.env.API_KEY,
 });
