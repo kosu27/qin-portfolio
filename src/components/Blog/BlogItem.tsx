dc5b812bcd7095ac047760f0264f6afc5d3f0f44
kosu <kosumon27@gmail.com>
2022-08-30 02:00:41 +0900

    🐛 修正

--- src/components/Blog/BlogItem.tsx
+++ src/components/Blog/BlogItem.tsx
@@ -2,7 +2,7 @@ import { Stack, Text, useMantineTheme } from "@mantine/core";
 import { dateFormatted } from "lib/dayJs/day";
 import { FC } from "react";
 import { Blog } from "types/Blog";
-import { textToHtml } from "utils/textToHtml";
+import { htmlToText } from "utils/textToHtml";
 
 type Props = {
   blog: Blog;
@@ -16,7 +16,7 @@ export const BlogItem: FC<Props> = ({ blog }) => {
       <Text my={0} component="h3" size={24}>
         {blog.title}
       </Text>
-      <Text size={16}>{textToHtml(blog.content)}</Text>
+      <Text size={16}>{htmlToText(blog.content)}</Text>
       <Text size={12} color={theme.colors.dark[2]} weight="bold">
         {dateFormatted(blog.publishedAt!, "YYYY/MM/DD")}
       </Text>
--- src/utils/textToHtml.ts
+++ src/utils/textToHtml.ts
@@ -1,11 +1,13 @@
 export const textToHtml = (text: string): string => {
   const lineFeedExp = /(\n)/g;
   let convertedText = text.replace(lineFeedExp, "<br>");
+
   const urlRegExp = /(https?:\/\/[\w!?/+\-_~;.,*&@#$%()'[\]]+)/g;
   convertedText = convertedText.replace(
     urlRegExp,
     '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>'
   );
+
   return convertedText;
 };
 
