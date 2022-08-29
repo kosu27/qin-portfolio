3290e3042f0e851f7a7db6566a4464a4c4201997
kosu <kosumon27@gmail.com>
2022-08-29 17:52:08 +0900

    🎨 text HTML

--- src/utils/textToHtml.ts
+++ src/utils/textToHtml.ts
@@ -1,12 +1,14 @@
 export const textToHtml = (text: string): string => {
   const lineFeedExp = /(\n)/g;
   let convertedText = text.replace(lineFeedExp, "<br>");
-
   const urlRegExp = /(https?:\/\/[\w!?/+\-_~;.,*&@#$%()'[\]]+)/g;
   convertedText = convertedText.replace(
     urlRegExp,
     '<a href="$1" target="_blank" rel="noopener noreferrer">$1</a>'
   );
-
   return convertedText;
+};
+
+export const htmlToText = (html: string): string => {
+  return html.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g, "");
 };
