b6fd414fa46f3391e19ecb9f320da26bf72a26ee
kosu <kosumon27@gmail.com>
2022-08-30 16:12:15 +0900

    🐛 コメントアウト

--- src/components/Blog/BlogItem.tsx
+++ src/components/Blog/BlogItem.tsx
@@ -2,7 +2,7 @@ import { Stack, Text, useMantineTheme } from "@mantine/core";
 import { dateFormatted } from "lib/dayJs/day";
 import { FC } from "react";
 import { Blog } from "types/Blog";
-import { htmlToText } from "utils/htmlTransform";
+// import { htmlToText } from "utils/htmlTransform";
 
 type Props = {
   blog: Blog;
@@ -17,7 +17,7 @@ export const BlogItem: FC<Props> = ({ blog }) => {
         {blog.title}
       </Text>
       <Text size={16} lineClamp={3}>
-        {htmlToText(blog.content)}
+        (blog.content)
       </Text>
       <Text size={12} color={theme.colors.dark[2]} weight="bold">
         {dateFormatted(blog.publishedAt!, "YYYY/MM/DD")}
