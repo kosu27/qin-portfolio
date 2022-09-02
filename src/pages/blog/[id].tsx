73d1216627d928c563dbb9b2e01ff340483bac63
kosu <kosumon27@gmail.com>
2022-09-02 16:59:42 +0900

    ✨ ブログ詳細ページ

---
+++ src/pages/blog/[id].tsx
@@ -0,0 +1,59 @@
+import { BlogDisplay } from "components/Blog";
+import { Layout } from "components/templates/Layout";
+import { client } from "lib/microCMS/client";
+import { GetStaticPaths, GetStaticProps, NextPage } from "next";
+import { Blog, BlogAnswer } from "types/Blog";
+
+type Props = {
+  blog: Blog;
+};
+
+type Path = {
+  id: string;
+};
+
+const BlogDetailPage: NextPage<Props> = ({ blog }) => {
+  return (
+    <Layout>
+      <BlogDisplay blog={blog} />
+    </Layout>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths<Path> = async () => {
+  const data: BlogAnswer = await client.get({
+    endpoint: "blog",
+  });
+  const blogs = data.contents;
+  const paths = blogs.map((blog) => {
+    return {
+      params: {
+        id: blog.id,
+      },
+    };
+  });
+  return { paths, fallback: "blocking" };
+};
+
+export const getStaticProps: GetStaticProps<Props> = async (ctx) => {
+  const id = ctx.params?.id as string;
+  try {
+    const blog: Blog = await client.get({
+      endpoint: "blog",
+      contentId: id,
+    });
+
+    return {
+      props: {
+        blog: blog,
+      },
+      revalidate: 60,
+    };
+  } catch {
+    return {
+      notFound: true,
+    };
+  }
+};
+
+export default BlogDetailPage;
