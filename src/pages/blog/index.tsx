afcdeb56471e7407758d703979b296cfc0c61e19
kosu <kosumon27@gmail.com>
2022-09-04 04:26:10 +0900

    ✅

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,25 +1,24 @@
 import { Center, Loader, Text } from "@mantine/core";
+import { Blogs } from "components/Blog";
 import { Layout } from "components/templates/Layout";
+import { client } from "lib/microCMS/client";
+import { useRequestBlog } from "lib/swr/useRequestBlog";
 import { GetStaticProps, NextPage } from "next";
 import InfiniteScroll from "react-infinite-scroller";
-import { Blogs } from "components/Blog";
-import { useRequestBlog } from "lib/swr/useRequestBlog";
-import { client } from "lib/microCMS/client";
 import { Blog } from "types/Blog";
 
 type Props = {
   blogs: Blog[];
 };
 
-const BlogPage: NextPage<Props> = ({ blogs }) => {
-  const { items, error, size, setSize, isLoadingMore, isReachingEnd } = useRequestBlog(blogs);
+const BlogIndex: NextPage<Props> = ({ blogs }) => {
+  const { items, error, isLoadingMore, size, setSize, isReachingEnd } = useRequestBlog(blogs);
 
   const loadMore = () => {
     if (!isLoadingMore && !isReachingEnd) {
       setSize(size + 1);
     }
   };
-
   if (error) {
     return (
       <Layout>
@@ -50,13 +49,18 @@ const BlogPage: NextPage<Props> = ({ blogs }) => {
 };
 
 export const getStaticProps: GetStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog", queries: { orders: "-publishedAt" } });
+  const data = await client.get({
+    endpoint: "blog",
+    queries: { orders: "-publishedAt" },
+  });
+
   const props: Props = {
     blogs: data.contents,
   };
+
   return {
     props: props,
   };
 };
 
-export default BlogPage;
+export default BlogIndex;
