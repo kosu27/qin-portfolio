d2388d54edc821fddc7798dded8e14878358d53a
kosu <kosumon27@gmail.com>
2022-09-12 03:44:06 +0900

    🔧 twitter client

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,5 +1,8 @@
 declare namespace NodeJS {
   interface ProcessEnv {
     readonly NEXT_PUBLIC_API_KEY: string;
+    readonly NEXT_PUBLIC_TWITTER_API_KEY: string;
+    readonly NEXT_PUBLIC_TWITTER_API_KEY_SECRET: string;
+    readonly NEXT_PUBLIC_TWITTER_BEARER_TOKEN: string;
   }
 }
