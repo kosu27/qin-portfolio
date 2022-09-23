6d2dc02a445ff915ac12940171b7a41baa755de4
kosu <kosumon27@gmail.com>
2022-09-23 19:42:53 +0900

    🐛

--- src/components/Github/index.ts
+++ src/components/Github/index.ts
@@ -1,3 +1,4 @@
 export { GithubData } from "./GithubData";
 export { GithubItem } from "./GithubItem";
 export { GithubRepositories } from "./GithubRepositories";
+export { GithubLanguage } from "./GithubLanguage";
