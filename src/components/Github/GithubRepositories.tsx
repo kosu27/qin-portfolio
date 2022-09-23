563b8788d031db69a036e249789c78aa0949c4f0
kosu <kosumon27@gmail.com>
2022-09-23 19:26:58 +0900

    ✨ CSR

--- src/components/Github/GithubRepositories.tsx
+++ src/components/Github/GithubRepositories.tsx
@@ -1,24 +1,43 @@
-import { Center, Stack } from "@mantine/core";
+import { useQuery } from "@apollo/client";
+import { Center, ScrollArea, Stack } from "@mantine/core";
 import { SegmentButton } from "components/Button";
 import { GithubItem } from "components/Github/GithubItem";
 import { SegmentTitle } from "components/Title";
+import { repositoryLanguagesQuery } from "lib/github/query.graphql";
+import { useMediaQuery } from "lib/mantine";
 import { FC } from "react";
-import { Github } from "types/Github";
+import { GetRepositoryLanguagesQuery } from "types/Github";
+import { Repository } from "types/Repository";
+import { queryRepositories } from "utils/repositoriesQuery";
 
 type Props = {
-  repositories: Github[];
+  repositories: Repository[];
 };
 
 export const GithubRepositories: FC<Props> = ({ repositories }) => {
-  const items = repositories.slice(0, 5);
+  const isDesktop = useMediaQuery("sm");
+  const { data } = useQuery<GetRepositoryLanguagesQuery>(repositoryLanguagesQuery, {
+    variables: { repositoriesFirst: 10, languagesFirst: 10 },
+  });
+
+  const items: Repository[] = [];
+  if (data) {
+    Array.prototype.push.apply(items, queryRepositories(data));
+  } else {
+    Array.prototype.push.apply(items, repositories);
+  }
 
   return (
     <Stack spacing={0}>
       <SegmentTitle>Github</SegmentTitle>
       <Stack spacing={24}>
-        {items.map((repository) => (
-          <GithubItem key={repository.id} github={repository} />
-        ))}
+        <ScrollArea style={{ height: isDesktop ? 880 : 400 }} pr={24}>
+          <Stack spacing={40}>
+            {items.map((repository) => (
+              <GithubItem key={repository.id} repository={repository} />
+            ))}
+          </Stack>
+        </ScrollArea>
         <Center mt={8}>
           <SegmentButton display="View on Github" />
         </Center>
